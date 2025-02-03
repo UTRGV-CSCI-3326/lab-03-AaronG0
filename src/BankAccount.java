d9ec65404915784c66c1b50668dada84e629c226
AaronG0 <150050732+AaronG0@users.noreply.github.com>
2025-02-03 00:39:30 +0000

    Bob's bank account update

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,13 @@
-
+public class BankAccount{
+    public static void main(String[] args){
+        double bobbalance = 2175.37;
+        bobbalance -= 302.50;
+        bobbalance += 50.03;
+        bobbalance /= 2.00;
+        bobbalance += 20.00;
+        bobbalance -= 1.00;
+        bobbalance *= 2.00;
+        bobbalance += 1.00;
+        System.out.printf("Bob's new balance is: " + "%.2f", bobbalance); // %.2f is an identifier to format the balance
+    }
+}
